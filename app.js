28814d85e565dd8ccdf2238e65f0499c1e9bbe3e
niatize <niatizepro@gmail.com>
2026-04-25 17:10:31 +0100

    Mise à jour du style CSS, ajout de nouvelles fonctionnalités JavaScript pour le quiz et création d'une nouvelle page HTML pour le chat. Ajout d'une image PNG pour l'interface.

--- app.js
+++ app.js
@@ -1,2 +1,43 @@
-let quest1=document.getElementById('reponse_2').value;
-quest1.style.display='bloc';
+const question=[
+    "bonjour a vous comment allez-vou? ",
+    "quelle est la superficie du cameroun ?",
+    "qui est le président du cameroun(Paul bathélémie;Emanuel macromp) ?",
+    "Quel sont les anciens constitutions de l'état Camerounais ?"
+
+]
+let tab=new Array(
+    "475.650 km²",
+    "Paul bathélémie",
+
+
+);
+
+let etape=0;
+
+let ecriture=document.getElementById("reponse");
+let envoie=document.getElementById('envoie');
+let contenue=document.getElementById("contenue");
+
+function add_message(texte,type){
+    const div=document.createElement('div');
+    div.classList.add("message",type);
+    div.textContent=texte;
+    contenue.appendChild(div);
+    contenue.scrollTop=contenue.scrollHeight;
+}
+window.onload=()=>{
+    add_message(question[etape],'recevoir');
+}
+function gestion_denvoie(){
+    const reponse=ecriture.value.trim();
+    if(reponse!=="" && etape < question.length-1){
+        add_message(reponse,'send');
+        ecriture.value="";
+        
+        setTimeout(()=>{
+            etape++;
+            add_message(question[etape],'recevoir')
+        },700)
+    }
+}
+envoie.addEventListener('click',gestion_denvoie);
